c83b13c480b6d49a9869cc3c7eb5093af069a226
Fan Li <60990939+fl08653n@users.noreply.github.com>
2020-03-26 20:06:35 -0400

    Add files via upload

---
+++ javascript5.js
@@ -0,0 +1,11 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.fillStyle="yellow";
+c.fillRect(100,100,100,100);
+c.fillStyle="red";
+c.fillRect(200,200,100,100);
+c.fillStyle="blue";
+c.fillRect(300,300,100,100);
